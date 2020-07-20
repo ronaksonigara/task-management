0b646e47291c2746fd379a91fd013376fa3cfa7e
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2020-07-20 13:00:47 +0530

    Updates

---
+++ precache-manifest.7d78a3ff3680e9987cf95702c2ddd4fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0676b534c5fd704202dfcd7728c1d66b",
+    "url": "/task-management/index.html"
+  },
+  {
+    "revision": "6fc033fb00a7a78203ef",
+    "url": "/task-management/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "af90190e88d08df0c714",
+    "url": "/task-management/static/css/main.08dc72df.chunk.css"
+  },
+  {
+    "revision": "6fc033fb00a7a78203ef",
+    "url": "/task-management/static/js/2.69247889.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/task-management/static/js/2.69247889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af90190e88d08df0c714",
+    "url": "/task-management/static/js/main.d76cf48f.chunk.js"
+  },
+  {
+    "revision": "53d192294bf35ac76051",
+    "url": "/task-management/static/js/runtime-main.eccd7469.js"
+  }
+]);
